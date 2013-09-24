f5765a83a88e0eb530b9f73d32a3edb8895a6834
Matt <matt.vanek@gmail.com>
2013-09-24 02:05:11 -0500

    Allow no-time queries for coupons.

--- handlers/coupon.py
+++ handlers/coupon.py
@@ -31,12 +31,15 @@ class CouponHandler(webapp2.RequestHandler):
             user_id = int(urllib.unquote(self.request.get('user')))
         except ValueError:
             user_id = None
-        month = int(urllib.unquote(self.request.get('month')))
-        day = int(urllib.unquote(self.request.get('day')))
-        year = int(urllib.unquote(self.request.get('year')))
-        hour = int(urllib.unquote(self.request.get('hour')))
-        minute = int(urllib.unquote(self.request.get('min')))
-        time = datetime.datetime(year, month, day, hour, minute)
+        try:
+            month = int(urllib.unquote(self.request.get('month')))
+            day = int(urllib.unquote(self.request.get('day')))
+            year = int(urllib.unquote(self.request.get('year')))
+            hour = int(urllib.unquote(self.request.get('hour')))
+            minute = int(urllib.unquote(self.request.get('min')))
+            time = datetime.datetime(year, month, day, hour, minute)
+        except ValueError:
+            time = None
         self.status = '200 OK'
         for key in Coupon.list_coupons(user_id=user_id,
                                        business_id=business_id,
