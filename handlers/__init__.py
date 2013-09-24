8fb784f5055a11e6a86dbb44bf9337493b2e93f3
Matt <matt.vanek@gmail.com>
2013-09-24 13:28:26 -0500

    Transform test panel to admin panel at /admin

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,5 @@
 from root import MainHandler
+from admin import AdminHandler
 from business import BusinessHandler, BusinessIDHandler
 from user import UserHandler, UserIDHandler
 from coupon import CouponHandler, CouponIDHandler
@@ -6,6 +7,7 @@ from coupon import CouponHandler, CouponIDHandler
 
 __all__ = [
     MainHandler,
+    AdminHandler,
     BusinessHandler, BusinessIDHandler,
     UserHandler, UserIDHandler,
     CouponHandler, CouponIDHandler,
