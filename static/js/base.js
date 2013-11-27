1d14104fe8b8e5ee11071cfad1e33323eb0a34c0
Matt <matt.vanek@gmail.com>
2013-11-27 02:20:25 -0600

    Add drop-down menu to header bar

---
+++ static/js/base.js
@@ -0,0 +1,28 @@
+var Dropdown = function( menuNode ) {
+    var submenuNode;
+
+    function find_submenu() {
+        var i;
+        for ( i=0; i<menuNode.childNodes.length; i++ ) {
+            if ( menuNode.childNodes[i].className == 'submenu' ) {
+                return menuNode.childNodes[i];
+            }
+        }
+    }
+
+    function drop() {
+    }
+
+    submenuNode = find_submenu();
+    menuNode.addEventListener('hover', drop, false);
+}
+
+jQuery( document ).ready(function() {
+    var i,
+        dropdownNodes;
+
+    dropdownNodes = jQuery('.dropdown');
+    for ( i=0; i<dropdownNodes.length; i++ ) {
+        new Dropdown( dropdownNodes[i] );
+    }
+})
